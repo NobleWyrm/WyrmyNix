c7b7e9e88c456c975a967885311c9b497e66e426
installer <installer@gmail.com>
2024-09-17 02:05:02 -0400

    Initial, nonfunctional commit. Cut me some slack, the Adderall shortage has been hard on us all.

---
+++ nixos/bootloader.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }:
+
+{
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.initrd.luks.devices."luks-c3413beb-aea3-4010-b9f4-61f9abd8ff1e".device = "/dev/disk/by-uuid/c3413beb-aea3-4010-b9f4-61f9abd8ff1e";
+  boot.plymouth = {
+    enable = true;
+    font = "${pkgs.jetbrains-mono}/share/fonts/truetype/JetBrainsMono-Regular.ttf";
+    themePackages = [ pkgs.catppuccin-plymouth ];
+    theme = "catppuccin-macchiato";
+  };
+}
