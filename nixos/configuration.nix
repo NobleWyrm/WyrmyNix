b7d429e4b61b05a11e40d9727b561a7b7168e6d3
installer <installer@gmail.com>
2024-09-18 13:26:37 -0400

    Added powertop and neofetch

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -151,7 +151,6 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     neovim
     tmux
@@ -159,6 +158,8 @@
     git
     btop
     killall
+    powertop
+    neofetch
 
     # NixOS Specific Tools
     alejandra
