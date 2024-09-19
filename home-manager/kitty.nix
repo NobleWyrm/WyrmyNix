008716ab296bed051d835c14468fbbd21a9b26d6
installer <installer@gmail.com>
2024-09-19 11:42:49 -0400

    Fixed the typo'd definition, added lib to the arguments so that mkForce could be used to overwrite the default config, and added a sickass coolguy banner to the top.

--- home-manager/kitty.nix
+++ home-manager/kitty.nix
@@ -1,13 +1,23 @@
+# ██╗  ██╗██╗████████╗████████╗██╗   ██╗
+# ██║ ██╔╝██║╚══██╔══╝╚══██╔══╝╚██╗ ██╔╝
+# █████╔╝ ██║   ██║      ██║    ╚████╔╝ 
+# ██╔═██╗ ██║   ██║      ██║     ╚██╔╝  
+# ██║  ██╗██║   ██║      ██║      ██║   
+# ╚═╝  ╚═╝╚═╝   ╚═╝      ╚═╝      ╚═╝   
+
 {
   config,
   pkgs,
+  lib,
   ...
 }: {
-  pkgs.kitty = {
+  programs.kitty = {
     enable = true;
     settings = {
+      # Gets rid of the annoying confirmation on close.
+      # No kitty, I don't care if there is an inactive bash session, just kill the pane!
       confirm_os_window_close = 0;
-      background_opacity = "0.75";
+      background_opacity = lib.mkForce "0.75";
       window_padding_width = 10;
       scrollback_lines = 10000;
       enable_audio_bell = false;
