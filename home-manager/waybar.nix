2e0ed9a24c067cffe288189c48918528f934d865
installer <installer@gmail.com>
2024-09-18 21:43:06 -0400

    Restored some more of the waybar configuration after stripping out the coloration stuff, gonna fiddle more in a bit.

--- home-manager/waybar.nix
+++ home-manager/waybar.nix
@@ -61,11 +61,6 @@
             tooltip = true;
             tooltip-icon-size = 24;
           }
-          #          {
-          #            type = "audio-out";
-          #            tooltip = true;
-          #            tooltip-icon-size = 24;
-          #          }
           {
             type = "audio-in";
             tooltip = true;
@@ -76,49 +71,36 @@
     };
   };
 
-  #  programs.waybar.style = ''
-  #
-  #    * {
-  #      border: none;
-  #      border-radius: 0;
-  #      padding: 0;
-  #      margin: 0;
-  #      font-size: 11px;
-  #    }
-  #
-  #    window#waybar {
-  #      background: #292828;
-  #      color: #0f8f4f;
-  #    }
-  #
-  #    #custom-logo {
-  #      font-size: 18px;
-  #      margin: 0;
-  #      margin-left: 7px;
-  #      margin-right: 16px;
-  #      padding: 0;
-  #      font-family: NotoSans Nerd Font Mono;
-  #    }
-  #
-  #    #workspaces button {
-  #      margin-right: 10px;
-  #      color: #ffffff;
-  #    }
-  #    #workspaces button:hover, #workspaces button:active {
-  #      background-color: #292828;
-  #      color: #ffffff;
-  #    }
-  #    #workspaces button.focused {
-  #      background-color: #383737;
-  #    }
-  #
-  #    #language {
-  #      margin-right: 7px;
-  #    }
-  #
-  #    #battery {
-  #      margin-left: 7px;
-  #      margin-right: 3px;
-  #    }
-  #  '';
+  programs.waybar.style = ''
+  
+    * {
+      border: none;
+      border-radius: 0;
+      padding: 0;
+      margin: 0;
+      font-size: 11px;
+    }
+  
+    #custom-logo {
+      font-size: 18px;
+      margin: 0;
+      margin-left: 7px;
+      margin-right: 16px;
+      padding: 0;
+      font-family: NotoSans Nerd Font Mono;
+    }
+  
+    #workspaces button {
+      margin-right: 10px;
+    }
+  
+    #language {
+      margin-right: 7px;
+    }
+  
+    #battery {
+      margin-left: 7px;
+      margin-right: 3px;
+    }
+  '';
 }
