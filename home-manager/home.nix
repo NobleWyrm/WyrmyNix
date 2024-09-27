3f6621677d6006d724dd4c8fa39ffc2c9d9e986b
Brian Wyrm <majikguy@gmail.com>
2024-09-26 21:03:50 -0400

    Added Teamspeak

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -48,6 +48,7 @@
     obsidian
     # Pipewire version of pavucontrol, handy to have.
     pwvucontrol
+    teamspeak_client
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
